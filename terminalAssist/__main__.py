ae3154e8d088591bd936f21393059c21726a346a
prashantha sp <prashanth.sp98@gmail.com>
2019-06-03 23:07:33 +0530

    Added user details

---
+++ terminalAssist/__main__.py
@@ -0,0 +1,16 @@
+import os
+import json
+
+if __name__ == "__main__":
+    data = {}
+    if not os.path.isfile(".user_details.json"):
+        print("Enter user details")
+        data['name'] = input("Username : ")
+        data['email'] = input("Email ID : ")
+        data['dob'] = input("Date Of Birth(dd/mm/yyyy) : ")
+
+        with open(".user_details.json", "w") as fo:
+            json.dump(data, fo)
+
+    if os.path.isfile(".user_details.json"):
+        pass
