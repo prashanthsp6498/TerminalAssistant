32c26967ab1e363d62da8bb40d1d6924216a5d08
prashantha sp <prashanth.sp98@gmail.com>
2019-06-04 10:46:05 +0530

    added autopep8 to auto format code

--- test.py
+++ test.py
@@ -1,4 +1,21 @@
 #! /usr/bin/python3
 import os
+import sys
 
-os.system("flake8 --max-line-length=90 terminalAssist/")
+if sys.argv[1] == '--h':
+    print('--auto\tAutomaticaly follow PEP8')
+    print('--run\tTo check standard')
+
+try:
+    if sys.argv[1] == '--auto':
+        os.system("autopep8 -i terminalAssist/")
+
+    if sys.argv[1] == '--run':
+        os.system("flake8 --max-line-length=90 terminalAssist/")
+        choice = input("Is code in PEP8 standard...? [y/n]")
+        if choice == 'n' or 'N':
+            os.system("autopep8 -i terminalAssist/*/*.py")
+
+        print("Code is checked")
+except Exception as e:
+    print(e)
