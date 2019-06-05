8b52a83972e4d497fb56bf3c7bdda99954d98c06
prashantha sp <prashanth.sp98@gmail.com>
2019-06-05 22:24:09 +0530

    commented userdetails

--- terminalAssist/__main__.py
+++ terminalAssist/__main__.py
@@ -4,7 +4,7 @@ import plugins.speedtest as speed
 
 if __name__ == "__main__":
     data = {}
-    if not os.path.isfile(".user_details.json"):
+    '''if not os.path.isfile(".user_details.json"):
         print("Enter user details")
         data['name'] = input("Username : ")
         data['email'] = input("Email ID : ")
@@ -12,6 +12,6 @@ if __name__ == "__main__":
 
         with open(".user_details.json", "w") as fo:
             json.dump(data, fo)
-
+    '''
     if os.path.isfile(".user_details.json"):
         speed.test_speed()
