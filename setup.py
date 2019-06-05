9b16ca1e3de1768789dc276b5ade719da3fcbf81
prashantha sp <prashanth.sp98@gmail.com>
2019-06-05 20:17:19 +0530

    Configured to new environment

--- setup.py
+++ setup.py
@@ -8,11 +8,13 @@ else:
     print("Python3 is not found")
     os.system("sudo apt-get install python3")
     os.system("sudo apt install python3-pip")
+os.system("sudo apt-get install python3-venv")
 os.system("sudo pip3 install -r requirements.txt")
 if os.path.isfile("assist"):
     os.system("rm assist")
 pwd = os.getcwd()
 with open("assist", "w") as fo:
+    fo.write("source "+pwd+"/assistant/bin/activate\n")
     fo.write("python3 "+pwd+"/terminalAssist/")
 os.system("sudo cp assist /usr/local/bin/")
-os.system("sudo chmod 777 assist /usr/local/bin/")
+os.system("sudo chmod 777 /usr/local/bin/assist")
