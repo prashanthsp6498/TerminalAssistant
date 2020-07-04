41c227a390fbb84872d99704e9e4603198cbcb08
kushtej <007kushtej@gmail.com>
2020-07-04 20:15:14 +0530

    completed user_details.json

--- assistant/__main__.py
+++ assistant/__main__.py
@@ -1,32 +1,60 @@
 import os
 import json
-import plugins.speedtest as speed
-
-
 import re
+
+
 def validate_email(email):
-  return bool(re.search(r"^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$", email))
+	if(bool(re.search(r"^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$", email)) is True):
+		data['email'] = email
+		return bool(re.search(r"^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$", email))
+	else:
+		print("enter correct email address")
+		exit(0)
+
 
 def validate_dob(dob):
-  import datetime
-  try:
-      datetime.datetime.strptime(dob,"%d/%m/%y")
-  except ValueError as err:
-      print(err)
+	import datetime
+	try:
+		date=dob.split("/")
+		datetime.datetime(year=int(date[2]),month=int(date[1]),day=int(date[0]),hour=0)
+		data['dob'] = dob
+	except ValueError as err:
+		print(err)
+		exit(0)
 
-validate_dob('33/09/2002')
 
-# if __name__ == "__main__":
-#     data = {}
-#     if not os.path.isfile("user_details.json"):
-#         print("Enter user details")
+def validate_city(city):
+	from geopy.geocoders import Nominatim
+	geolocator = Nominatim(user_agent="assist")
+	try:
+		location = geolocator.geocode(city)
+		choice=input("\n Is your city Address "+str(location.address)+" (Yy/Nn) :  ")
+		if(choice == 'y' or choice == 'Y'):
+			city_lst =[]
+			city_lst.append(location.address)
+			city_lst.append(location.latitude)
+			city_lst.append(location.longitude)
+			data['city']=city_lst
 
-#         data['name'] = input("Username : ")
-#         data['email'] = input("Email ID : ")
-#         data['dob'] = input("Date Of Birth(dd/mm/yyyy) : ")
+		elif(choice == 'N' or choice == 'n'):
+			print("please enter correct city name")
+	except:
+			print("Please enter correct city or some functions may not be available")
 
-#         with open("user_details.json", "w") as fo:
-#             json.dump(data, fo)
-    
-#     if os.path.isfile("user_details.json"):
-#         speed.test_speed()
+
+if __name__ == "__main__":
+
+	data = {}
+	if not os.path.isfile("user_details.json"):
+		print("Enter user details")
+		
+		data['name'] = input("Username : ")
+		validate_email(input("Enter Email :"))
+		validate_dob(input("Enter date of Birth(dd/mm/yyyy) :"))
+		validate_city(input("Enter your city name : "))
+		
+		with open("user_details.json", "w") as fo:
+			json.dump(data, fo)
+
+		# if os.path.isfile("user_details.json"):
+	    #     speed.test_speed()
--- assistant/test.py
+++
@@ -1,7 +0,0 @@
-# from plugins import jokes
-# jokes.joke()
-
-# from plugins import bitcoin_price
-# bitcoin_price.price()
-
-from plugins import speedtest
