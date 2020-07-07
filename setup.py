1b160a3e2d273ba3440e3db1918df6623639268e
kushtej <007kushtej@gmail.com>
2020-07-07 17:48:02 +0530

    added assist to setup.py file

--- setup.py
+++ setup.py
@@ -14,8 +14,11 @@ if os.path.isfile("assist"):
     os.system("rm assist")
 pwd = os.getcwd()
 with open("assist", "w") as fo:
-    fo.write("source "+pwd+"/venv/bin/activate\n")
-    fo.write("python3 "+pwd+"/assistant/")
+    fo.write("#! /usr/bin/python3\n\nimport os\nimport sys\n")
+    fo.write("if(len(sys.argv)>1):\n\t")
+    # fo.write("source "+pwd+"/venv/bin/activate\n")
+    fo.write("os.system(\"python3 "+pwd+"/assistant/\"+str(sys.argv[1:]))\n")
+    fo.write("else:\n\tprint(\"usage: assist [--version] [--help] [--features]\")")
 os.system("sudo mv assist /usr/local/bin/")
 os.system("sudo chmod +x /usr/local/bin/assist")
 
