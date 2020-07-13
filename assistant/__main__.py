f0207b747afbd1df30e0e2bca9006ed2f0581444
kushtej <007kushtej@gmail.com>
2020-07-13 22:25:10 +0530

    followed pep8 std

--- assistant/__main__.py
+++ assistant/__main__.py
@@ -10,23 +10,24 @@ if __name__ == "__main__":
     if os.path.isfile("/usr/local/bin/.user_details.json"):
         f = open("/usr/local/bin/.user_details.json", 'r')
         user_details = json.load(f)
-        print("\nHey "+user_details['name']+"!\t\t\t"+"Day : "+datetime.now().strftime('%B %d,%Y, %H:%M:%S')+"\n")
+        print("\nHey "+user_details['name']+"!\t\t\t"+"Day : " +
+              datetime.now().strftime('%B %d,%Y, %H:%M:%S')+"\n")
 
         assist_args = [re.sub(r'[[\]]', '', i) for i in sys.argv[1:]]
-        
-        if(assist_args[0] == '--features'):         
+
+        if(assist_args[0] == '--features'):
             assist_instructions.features()
 
-        if(assist_args[0] == '--help'):            
+        if(assist_args[0] == '--help'):
             assist_instructions.help()
 
-        if(assist_args[0] == '--version'):            
+        if(assist_args[0] == '--version'):
             assist_instructions.version()
 
-        if(assist_args[0] == '--joke'):           
+        if(assist_args[0] == '--joke'):
             jokes.joke()
 
-        if(assist_args[0] == '--bitcoin-price'):           
+        if(assist_args[0] == '--bitcoin-price'):
             bitcoin_price.price()
 
         if(assist_args[0] == '--quotes'):
@@ -44,10 +45,10 @@ if __name__ == "__main__":
                                 user_details['city']["longitude"])
 
         if(assist_args[0] == '--dictionary'):
-            word=input("Enter the Word : ")
+            word = input("Enter the Word : ")
             en_dictionary.meaning(word)
-        
+
         if(assist_args[0] == '--currency'):
             currency_exchange.currency()
-        elif('--currency' and '--setbase' in assist_args ):
+        elif('--currency' and '--setbase' in assist_args):
             user_details.currency_setbase()
